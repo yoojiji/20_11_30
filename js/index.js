6aa3262268e90bc0dd2bcb9c5a17da4439bf15c8
yoojiji <62611208+yoojiji@users.noreply.github.com>
2020-11-30 01:19:49 +0900

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,57 @@
+$(document).ready(function () { console.log("jquery complete") })
+
+
+$(function () {
+    $('.wave').click(function () {
+        $('.wave')
+    });
+});
+
+$(document).ready(function () {
+    $('#1').click(function () {
+        $('#demo').append('<embed id="embed_player" src="audio/사운드 1.wav" autostart="true" hidden="true"></embed>');
+    });
+});
+
+$(document).ready(function () {
+    $('#2').click(function () {
+        $('#demo').append('<embed id="embed_player" src="audio/사운드 2.wav" autostart="true" hidden="true"></embed>');
+    });
+});
+
+$(document).ready(function () {
+    $('#3').click(function () {
+        $('#demo').append('<embed id="embed_player" src="audio/사운드 3.wav" autostart="true" hidden="true"></embed>');
+    });
+});
+
+$(document).ready(function () {
+    $('#4').click(function () {
+        $('#demo').append('<embed id="embed_player" src="audio/사운드 4.wav" autostart="true" hidden="true"></embed>');
+    });
+});
+
+$(document).ready(function () {
+    $('#5').click(function () {
+        $('#demo').append('<embed id="embed_player" src="audio/사운드 5.wav" autostart="true" hidden="true"></embed>');
+    });
+});
+
+$(document).ready(function () {
+    $('#6').click(function () {
+        $('#demo').append('<embed id="embed_player" src="audio/사운드 6.wav" autostart="true" hidden="true"></embed>');
+    });
+});
+
+$(document).ready(function () {
+    $('#7').click(function () {
+        $('#demo').append('<embed id="embed_player" src="audio/사운드 7.wav" autostart="true" hidden="true"></embed>');
+    });
+});
+
+$(document).ready(function () {
+    $('#8').click(function () {
+        $('#demo').append('<embed id="embed_player" src="audio/사운드 8.wav" autostart="true" hidden="true"></embed>');
+    });
+});
+
